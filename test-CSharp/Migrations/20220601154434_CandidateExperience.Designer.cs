52b74efd31dfb8e61c8677b745bba6d4cee6023b
MarcelloBronzatti <mbsimogaki@gmail.com>
2022-06-01 12:45:57 -0300

    MIgracao

--- test-CSharp/Migrations/20220601154434_CandidateExperience.Designer.cs
+++ test-CSharp/Migrations/20220601154434_CandidateExperience.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using test_CSharp.Data;
 
@@ -11,9 +12,10 @@ using test_CSharp.Data;
 namespace test_CSharp.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220601154434_CandidateExperience")]
+    partial class CandidateExperience
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
