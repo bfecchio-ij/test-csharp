12aa94ae6f89aa96911ac8cb7786cf5948cd9f3c
Caio Coutinho <caiocoutinho34r@gmail.com>
2022-06-08 00:24:55 -0300

    Delete GestaoDeCandidatos/GestaoDeCandidatos directory

--- GestaoDeCandidatos/GestaoDeCandidatos/Models/ViewModels/CandidatesViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GestaoDeCandidatos.Models.ViewModels
-{
-    public class CandidatesViewModel
-    {
-        public Candidates candidates { get; set; }
-        public CandidateExperiences experiences { get; set; }
-        public List<Candidates> listCandidates { get; set; }
-    }
-}
