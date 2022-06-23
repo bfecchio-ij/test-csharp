7911de0501d3ffb69f6563a73f1514f242be078e
luqonhas <apolinariodev@gmail.com>
2022-06-23 01:30:08 -0300

    Solução e Domains criadas!

---
+++ InfoJobs/InfoJobs.Domain/Entities/Candidate.cs
@@ -0,0 +1,72 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using InfoJobs.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoJobs.Domain.Entities
+{
+    public class Candidate : Base
+    {
+        public Candidate(string name, string surname, DateTime birthdate, string email, DateTime insertDate, DateTime modifyDate)
+        {
+            AddNotifications(
+            new Contract<Notification>()
+                .Requires()
+                .IsNotEmpty(name, "Name", "The 'Name' field cannot be empty!")
+                .IsNotEmpty(surname, "Surname", "The 'Surname' field cannot be empty!")
+                .IsNotNull(birthdate, "Birthdate", "The 'Birthdate' field cannot be null!")
+                .IsEmail(email, "Email", "Enter a valid email address!")
+                .IsNotNull(insertDate, "InsertDate", "The 'InsertDate' field cannot be null!")
+            );
+
+            if (IsValid)
+            {
+                Name = name;
+                Surname = surname;
+                Birthdate = birthdate;
+                Email = email;
+                InsertDate = DateTime.Now;
+            }
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public DateTime Birthdate { get; set; }
+        public string Email { get; private set; }
+        public DateTime InsertDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+
+        // Compositions
+        public IReadOnlyCollection<CandidateExperience> Experiences { get; private set; }
+        private List<CandidateExperience> _experiences { get; set; }
+
+        // Configs
+
+        //public void RegisterExperience(CandidateExperience experience)
+        //{
+        //    if (IsValid)
+        //    {
+        //        _experiences.Add(experience);
+        //    }
+        //}
+
+        public void UpdateEmail(string email)
+        {
+            AddNotifications(
+            new Contract<Notification>()
+                .Requires()
+                .IsEmail(email, "Email", "Enter a valid email address!")
+            );
+
+            if (IsValid)
+            {
+                Email = email;
+            }
+        }
+
+    }
+}
---
+++ InfoJobs/InfoJobs.Domain/Entities/CandidateExperience.cs
@@ -0,0 +1,67 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using InfoJobs.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoJobs.Domain.Entities
+{
+    public class CandidateExperience : Base
+    {
+        public CandidateExperience(string company, string job, string description, decimal salary, DateTime beginDate, DateTime insertDate, Guid idCandidates)
+        {
+            AddNotifications(
+            new Contract<Notification>()
+                .Requires()
+                .IsNotEmpty(company, "Company", "The 'Company' field cannot be empty!")
+                .IsNotEmpty(job, "Job", "The 'Job' field cannot be empty!")
+                .IsNotEmpty(description, "Description", "The 'Description' field cannot be empty!")
+                .IsNotNull(salary, "Salary", "The 'Salary' field cannot be null!")
+                .IsNotNull(beginDate, "BeginDate", "The 'BeginDate' field cannot be null!")
+                .IsNotNull(insertDate, "InsertDate", "The 'InsertDate' field cannot be null!")
+                .IsNotNull(idCandidates, "IdCandidates", "The 'IdCandidates' field cannot be null!")
+            );
+
+            Company = company;
+            Job = job;
+            Description = description;
+            Salary = salary;
+            BeginDate = beginDate;
+            InsertDate = insertDate;
+            IdCandidates = idCandidates;
+        }
+
+        public string Company { get; private set; }
+        public string Job { get; private set; }
+        public string Description { get; private set; }
+        public decimal Salary { get; private set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime InsertDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+
+        // FK's
+        public Guid IdCandidates { get; set; }
+        public Candidate Candidates { get; set; }
+
+
+        public void UpdateCompany(string company)
+        {
+            AddNotifications(
+            new Contract<Notification>()
+                .Requires()
+                .IsNotNull(company, "Company", "The 'Company' field cannot be null!")
+                .IsNotEmpty(company, "Company", "The 'Company' field cannot be empty!")
+            );
+
+            if (IsValid)
+            {
+                Company = company;
+            }
+        }
+
+    }
+}
